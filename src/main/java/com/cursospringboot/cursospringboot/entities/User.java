f1e86ba8886ea52cd0222a3124af7302a1a8a7b6
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-10-25 20:46:56 -0300

    testando o banco de dados h2

--- src/main/java/com/cursospringboot/cursospringboot/entities/User.java
+++ src/main/java/com/cursospringboot/cursospringboot/entities/User.java
@@ -14,84 +14,82 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-private String name;
-private String email;
-private String phone;
-private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 
-public User() {
-	
-}
+	public User() {
 
-public User(Long id, String name, String email, String phone, String password) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.email = email;
-	this.phone = phone;
-	this.password = password;
-}
+	}
 
-public Long getId() {
-	return id;
-}
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
-public void setId(Long id) {
-	this.id = id;
-}
+	public Long getId() {
+		return id;
+	}
 
-public String getName() {
-	return name;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public String getName() {
+		return name;
+	}
 
-public String getEmail() {
-	return email;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public void setEmail(String email) {
-	this.email = email;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public String getPhone() {
-	return phone;
-}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-public void setPhone(String phone) {
-	this.phone = phone;
-}
+	public String getPhone() {
+		return phone;
+	}
 
-public String getPassword() {
-	return password;
-}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-public void setPassword(String password) {
-	this.password = password;
-}
+	public String getPassword() {
+		return password;
+	}
 
-@Override
-public int hashCode() {
-	return Objects.hash(id);
-}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	User other = (User) obj;
-	return Objects.equals(id, other.id);
-}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
