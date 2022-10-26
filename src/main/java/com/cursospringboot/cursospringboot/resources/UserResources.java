823cc71a619a00ef5ae7f3d98353f8a04cf4b35e
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-10-25 23:28:04 -0300

    component registration

--- src/main/java/com/cursospringboot/cursospringboot/resources/UserResources.java
+++ src/main/java/com/cursospringboot/cursospringboot/resources/UserResources.java
@@ -1,19 +1,33 @@
 package com.cursospringboot.cursospringboot.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursospringboot.cursospringboot.entities.User;
+import com.cursospringboot.cursospringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 
 public class UserResources {
+	@Autowired
+	private UserService service;
+	
 @GetMapping	
-public ResponseEntity<User> findAll(){
-	User u = new User(1L, "carlos", "carlos@gmail.com", "99999999", "1234");
-	return ResponseEntity.ok().body(u);
+public ResponseEntity<List<User>> findAll(){
+List<User> list = service.findAll();
+	return ResponseEntity.ok().body(list);
+}
+@GetMapping(value="/{id}")
+public ResponseEntity<User>findById(@PathVariable Long id){
+	User obj = service.findById(id);
+	return ResponseEntity.ok().body(obj);
 }
 }
