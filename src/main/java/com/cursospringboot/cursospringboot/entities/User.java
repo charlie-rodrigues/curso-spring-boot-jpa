708f40d631ade8fa95c2cad5be7e95577ccefa7d
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-10-26 22:52:49 -0300

    part 1 order and instant

--- src/main/java/com/cursospringboot/cursospringboot/entities/User.java
+++ src/main/java/com/cursospringboot/cursospringboot/entities/User.java
@@ -1,12 +1,15 @@
 package com.cursospringboot.cursospringboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy="client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -74,6 +79,9 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,5 +99,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
