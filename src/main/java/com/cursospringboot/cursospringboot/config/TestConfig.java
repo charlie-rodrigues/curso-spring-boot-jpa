c73cc922a8922cc757a1e62f2cfea214154d0b33
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-10-29 21:01:58 -0300

    order status

--- src/main/java/com/cursospringboot/cursospringboot/config/TestConfig.java
+++ src/main/java/com/cursospringboot/cursospringboot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursospringboot.cursospringboot.entities.Order;
 import com.cursospringboot.cursospringboot.entities.User;
+import com.cursospringboot.cursospringboot.enums.OrderStatus;
 import com.cursospringboot.cursospringboot.repositories.OrderRepository;
 import com.cursospringboot.cursospringboot.repositories.UserRepository;
 
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "charlie", "charlie@gmail", "88998877", "1234");
 		User u2 = new User(null, "larissa", "larissa@gmail", "99889977", "1236");
 		
-		Order o1 = new Order(null, Instant.parse("2022-10-22T22:22:22Z"),u1);
-		Order o2 = new Order(null,Instant.parse("2002-09-23T23:44:12Z"),u2);
-		Order o3 = new Order(null, Instant.parse("2019-04-29T10:00:00Z"),u1);
+		Order o1 = new Order(null, Instant.parse("2022-10-22T22:22:22Z"),OrderStatus.CANCELED,  u1);
+		Order o2 = new Order(null,Instant.parse("2002-09-23T23:44:12Z"),OrderStatus.DELIVERED,u2);
+		Order o3 = new Order(null, Instant.parse("2019-04-29T10:00:00Z"),OrderStatus.WAITING_PAYMENT,u1);
 		userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepsitory.saveAll(Arrays.asList(o1,o2,o3));
 	}
