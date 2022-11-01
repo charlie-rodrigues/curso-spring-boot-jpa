c7d7f049700718ff03b614b3b5c4a2386f935fe6
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-11-01 17:29:51 -0300

    muitos para muitos e inner join com jpa

--- src/main/java/com/cursospringboot/cursospringboot/config/TestConfig.java
+++ src/main/java/com/cursospringboot/cursospringboot/config/TestConfig.java
@@ -2,12 +2,10 @@ package com.cursospringboot.cursospringboot.config;
 
 import java.time.Instant;
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
 import com.cursospringboot.cursospringboot.entities.Category;
 import com.cursospringboot.cursospringboot.entities.Order;
 import com.cursospringboot.cursospringboot.entities.Product;
@@ -25,33 +23,42 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepsitory;
-	@Autowired 
+	@Autowired
 	CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Category cat1 = new Category(null,"eletrinic");
-		Category cat2 = new Category(null,"movies");
-		Category cat3 = new Category(null,"music");
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		
+
+		Category cat1 = new Category(null, "eletrinic");
+		Category cat2 = new Category(null, "movies");
+		Category cat3 = new Category(null, "music");
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-			productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-			
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+
 		User u1 = new User(null, "charlie", "charlie@gmail", "88998877", "1234");
 		User u2 = new User(null, "larissa", "larissa@gmail", "99889977", "1236");
-		
-		Order o1 = new Order(null, Instant.parse("2022-10-22T22:22:22Z"),OrderStatus.CANCELED,  u1);
-		Order o2 = new Order(null,Instant.parse("2002-09-23T23:44:12Z"),OrderStatus.DELIVERED,u2);
-		Order o3 = new Order(null, Instant.parse("2019-04-29T10:00:00Z"),OrderStatus.WAITING_PAYMENT,u1);
+
+		Order o1 = new Order(null, Instant.parse("2022-10-22T22:22:22Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2002-09-23T23:44:12Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-04-29T10:00:00Z"), OrderStatus.WAITING_PAYMENT, u1);
 		userRepository.saveAll(Arrays.asList(u1, u2));
-        orderRepsitory.saveAll(Arrays.asList(o1,o2,o3));
+		orderRepsitory.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
--- src/main/java/com/cursospringboot/cursospringboot/entities/Category.java
+++ src/main/java/com/cursospringboot/cursospringboot/entities/Category.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable{
@@ -20,7 +22,8 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	
