93bb0e58e0d0962221ce6f466e19a7af6dd44dcc
charlie rodrigues <rodriguescharlyston@gmail.com>
2022-11-06 19:40:55 -0300

    exception delete

--- src/main/java/com/cursospringboot/cursospringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/cursospringboot/cursospringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cursospringboot.cursospringboot.services.exceptions.DataBaseException;
 import com.cursospringboot.cursospringboot.services.exceptions.ResourseNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,4 +21,15 @@ public class ResourceExceptionHandler {
     StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
 	return ResponseEntity.status(status).body(err);
 	}
+	
+	
+		@ExceptionHandler(DataBaseException.class)
+		public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
+	    String error = "Data base error!";
+	    HttpStatus status = HttpStatus.BAD_REQUEST;
+	    StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		}
+	
+	
 }
--- src/main/java/com/cursospringboot/cursospringboot/services/UserService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/UserService.java
@@ -4,38 +4,54 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursospringboot.cursospringboot.entities.User;
 import com.cursospringboot.cursospringboot.repositories.UserRepository;
+import com.cursospringboot.cursospringboot.services.exceptions.DataBaseException;
 import com.cursospringboot.cursospringboot.services.exceptions.ResourseNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
-private UserRepository repository;
-	public List<User>findAll(){
+	private UserRepository repository;
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourseNotFoundException(id));
 	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourseNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
-	public User update (Long id, User obj) {
+
+	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
+		updateData(entity, obj);
 		return repository.save(entity);
 	}
+
 	private void updateData(User entity, User obj) {
-     entity.setName(obj.getName());
-     entity.setEmail(obj.getEmail());
-     entity.setPhone(obj.getPhone());
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
-	
+
 }
